d454fdccd7f0a85b7e85ee4b32b0367a614b596d
Julio Cesar <jubslopes1812@hotmail.com>
2019-10-21 15:11:48 -0300

    Criar view doação

--- doacao/views.py
+++ doacao/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from doacao.models import Doacao
+from doacao.serializers import DoacaoSerializer
+
+
+class DoacaoViewSet(viewsets.ModelViewSet):
+    queryset = Doacao.objects.all()
+    serializer_class = DoacaoSerializer
