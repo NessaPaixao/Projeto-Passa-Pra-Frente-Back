614f559d639bd509774a32e5471a9e08a9e6c701
julio <jubsdrops1812@hotmail.com>
2019-10-21 14:43:25 -0300

    criar views e url

--- doador/serializers.py
+++ doador/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from doador.models import Doador
 
 
-class DoadorSerializer(serializerModelSerializer):
+class DoadorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doador
         fields = ('nome','id','idade','cpf','email','senha')
