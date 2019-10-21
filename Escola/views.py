19653b4743e50b38d89ffdca823fed997129c987
NessaPaixao <vanessadapaixaobarros1@gmail.com>
2019-10-21 14:56:50 -0300

    criar views Escola

--- Escola/views.py
+++ Escola/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from Escola.models import Escola
+from Escola.serializers import EscolaSerializer
+
+
+class EscolaViewSet(viewsets.ModelViewSet):
+    queryset = Escola.objects.all()
+    serializer_class = EscolaSerializer
