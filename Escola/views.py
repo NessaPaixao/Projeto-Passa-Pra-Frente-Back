00169adf3d75a8b81488d92fc0640dd6c8bb03dc
Julio Cesar <jubslopes1812@hotmail.com>
2019-10-22 15:51:51 -0300

    Adicionar actions na view

--- Escola/views.py
+++ Escola/views.py
@@ -14,4 +14,22 @@ class EscolaViewSet(viewsets.ModelViewSet):
     queryset = Escola.objects.all()
     serializer_class = EscolaSerializer
 
-    
+    @action(detail=True, methods=['POST'])
+    def aceitar(self, request, pk=None):
+        doacao = Doacao.objects.get(pk=request.data['doacao'])
+        doacao.escola = self.get_object()
+        # doacao.escola = None
+        doacao.save(update_fields=["escola"])
+        return Response({'messege': 'doado!'})
+
+    @action(detail=True, methods=["POST"])
+    def cancelar(self, request, pk=None):
+        doacao = Doacao.objects.get(pk=request.data['doacao'])
+        doacao.escola = None
+        doacao.save(update_fields=["escola"])
+        return Response({'messege': 'Cancelado a doação'})
+
+    @action(detail=True, methods=["GET"])
+    def aceitados(self, request, pk=None):
+        doacao = Doacao.objects.all().filter(escola=pk)
+        return Response(DoacaoSerializer(doacao, many=True).data)
