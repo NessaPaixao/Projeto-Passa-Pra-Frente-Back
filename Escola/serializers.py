d49a90cc7e3ed6bf6dcf16d3b1be20c5566341f5
Julio Cesar <jubslopes1812@hotmail.com>
2019-10-22 17:29:47 -0300

    Criar serializer

---
+++ Escola/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from Escola.models import Escola
+
+
+class EscolaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Escola
+        fields = ('id','nome_responsavel','cpf_responsavel', 'nome_escola', 'cep', 'telefone', 'usuario','senha')
+
+
+
+
+
+
