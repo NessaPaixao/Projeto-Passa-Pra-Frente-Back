f204790319222f28ef6d24433693a4bfa07c5347
Julio Cesar <jubslopes1812@hotmail.com>
2019-10-22 16:11:19 -0300

    Ajustar novos erros

--- Escola/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from Escola.models import Escola
-
-
-class EscolaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Escola
-        fields = ('id','nome_responsavel','cpf_responsavel', 'nome_escola', 'cep', 'telefone', 'usuario','senha')
--- doador/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from doador.models import Doador
-
-
-class DoadorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Doador
-        fields = ('id','nome','idade','cep','cpf','email','senha')
